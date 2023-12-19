b5a8ee247cbf9fbd980560204a9cc7a762ffcfed
sthairno <sthairno@outlook.jp>
2023-12-19 15:38:44 +0900

    MovingLampの微調整

--- Assets/Scripts/ARMainPanel.cs
+++ Assets/Scripts/ARMainPanel.cs
@@ -5,6 +5,16 @@ using UnityEngine.EventSystems;
 
 public class ARMainPanel : MonoBehaviour, IDragHandler
 {
+    /// <summary>
+    /// 1秒ごとのランプの数
+    /// </summary>
+    public float LampCountPerSecond = 10;
+
+    /// <summary>
+    /// ランプを表示する半径
+    /// </summary>
+    public float LampSpawnRadius = 50;
+
     [SerializeField]
     MovingLamp m_movingLampPrefab;
 
@@ -19,12 +29,14 @@ public class ARMainPanel : MonoBehaviour, IDragHandler
         if (data.button == PointerEventData.InputButton.Left)
         {
             var removedLamps = m_particles.RemoveLamp(data.position, 0.6f);
+            var delayMax = removedLamps.Length / LampCountPerSecond;
             foreach (var lamp in removedLamps)
             {
                 var movingLamp = Instantiate(m_movingLampPrefab, transform.parent);
                 movingLamp.Initialise(
-                    startPosition: data.position + Random.insideUnitCircle * 80f,
+                    startPosition: data.position + Random.insideUnitCircle * LampSpawnRadius,
                     target: m_collectAnimationTarget,
+                    delay: Random.Range(0, delayMax),
                     lamp: lamp
                 );
             }
--- Assets/Scripts/MovingLamp.cs
+++ Assets/Scripts/MovingLamp.cs
@@ -6,41 +6,72 @@ using UnityEngine.UIElements.Experimental;
 
 public class MovingLamp : MonoBehaviour
 {
-    public float Duration = 0.4f;
+    public float MoveDuration = 0.3f;
+
+    public float FadeInDuration = 0.02f;
 
     RectTransform m_rectTransform;
+
+    Image m_image;
 
     Vector2 m_startPos;
 
     Vector2 m_targetPos;
 
-    float m_time = 0;
+    float m_time;
 
     void Awake()
     {
         m_rectTransform = GetComponent<RectTransform>();
+        m_image = GetComponent<Image>();
     }
 
     void Update()
     {
         m_time += Time.deltaTime;
-        if (m_time > Duration)
+
+        float alphaT;
+        float posT;
+
+        if (m_time < 0)
+        {
+            return;
+        }
+        else if (m_time < FadeInDuration)
+        {
+            alphaT = m_time / FadeInDuration;
+            posT = 0;
+        }
+        else if (m_time <= FadeInDuration + MoveDuration)
+        {
+            alphaT = 1;
+            posT = Easing.InCirc((m_time - FadeInDuration) / MoveDuration);
+        }
+        else
         {
             Destroy(gameObject);
             return;
         }
 
-        float t = Easing.InCirc(m_time / Duration);
-        m_rectTransform.anchoredPosition = Vector2.Lerp(m_startPos, m_targetPos, t);
+        Color color = m_image.color;
+        color.a = alphaT;
+        m_image.color = color;
+
+        m_rectTransform.anchoredPosition = Vector2.Lerp(m_startPos, m_targetPos, posT);
     }
 
-    public void Initialise(Vector2 startPosition, RectTransform target, Lamp lamp)
+    public void Initialise(Vector2 startPosition, RectTransform target, float delay, Lamp lamp)
     {
-        m_rectTransform.anchoredPosition = startPosition;
         m_startPos = startPosition;
-
         m_targetPos = target.position;
+        m_time = -Mathf.Max(delay, 0);
 
-        GetComponent<Image>().color = lamp.Color;
+        m_image.color = new Color(
+            lamp.Color.r, 
+            lamp.Color.g, 
+            lamp.Color.b, 
+            a: delay <= 0.0f ? 1.0f : 0.0f
+        );
+        m_rectTransform.anchoredPosition = startPosition;
     }
 }
